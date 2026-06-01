d7eb64e4e799abe3ab49babcba0714883d761309
Ahmet Cilingir <face4forev@icloud.com>
2026-06-01 12:25:06 +0200

    Add Firebase authentication

--- TaskPoint/LoginViewController.swift
+++ TaskPoint/LoginViewController.swift
@@ -5,20 +5,83 @@
 //  Created by Ahmet CILINGIR on 30.05.26.
 //
 
+import FirebaseAuth
 import UIKit
 
 class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
+    @IBAction func signInButtonClicked(_ sender: Any) {
+        if let username = usernameTextField.text {
+            if let password = passwordTextField.text {
+                Auth.auth().signIn(withEmail: username, password: password) { data, error in
+                    if error != nil {
+                        //print(error?.localizedDescription)
+                        self.makeAlert(
+                            alertTitle: "SignIn Error",
+                            alertMessage: error?.localizedDescription
+                                ?? "Unknown Error"
+                        )
+                    } else {
+                        //print("success login")
+                        self.performSegue(
+                            withIdentifier: "toTaskListViewController",
+                            sender: nil
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        if let username = usernameTextField.text {
+            if let password = passwordTextField.text {
+                Auth.auth().createUser(withEmail: username, password: password) { data, error in
+                    if error != nil {
+                        //print(error?.localizedDescription)
+                        self.makeAlert(
+                            alertTitle: "SignUp Error",
+                            alertMessage: error?.localizedDescription
+                                ?? "Unknown Error"
+                        )
+                    } else {
+                        //print("success")
+                        self.performSegue(
+                            withIdentifier: "toTaskListViewController",
+                            sender: nil
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    func makeAlert(alertTitle: String, alertMessage: String) {
+        let alert = UIAlertController(
+            title: alertTitle,
+            message: alertMessage,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        let button = UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default
+        )
+        alert.addAction(button)
+        present(alert, animated: true)
+    }
 
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
